66cf5bbb18c7d510a41cb47a4fe0a3860979a929
karan <64732336+karan200@users.noreply.github.com>
2020-06-25 16:54:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,20 +5,17 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
-
 var form, player, game;
-
 var cars, car1, car2, car3, car4;
-
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  track = loadImage("image/track.jpg");
+  car1_img = loadImage("image/car1.png");
+  car2_img = loadImage("image/car2.png");
+  car3_img = loadImage("image/car3.png");
+  car4_img = loadImage("image/car4.png");
+  ground = loadImage("image/ground.png");
 }
 
 function setup(){
